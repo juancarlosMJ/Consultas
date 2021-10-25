7a692001ce538e31965b56cc25896247bfca038a
juancarlosMJ <mendez_30_yo@hotmail.com>
2021-10-25 10:59:19 -0500

    'practicas'

---
+++ eloquent/database/migrations/2021_10_25_133718_create_agenda_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
+
+class CreateAgendaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agenda', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_completo');
+            $table->string('telefono');
+            $table->string('email')-> unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agenda');
+    }
+}
